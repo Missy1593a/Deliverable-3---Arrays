f41ae232b0c90bda5e71dfff5ce0caeaa53b91ff
Amnay, Amrita <amrita2@usf.edu>
2019-09-19 15:20:23 -0400

    Changes

---
+++ Deliverable 3 - Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Author: Amrita Amnay
+    Date: 9/17/2019
+    Comments: This C# Console application code demonstrates the use of
+              Arrays
+*/
+using System;
+using System.Linq;
+namespace Deliverable_3___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] arr1 = Enumerable.Range(1, 25).ToArray();
+                foreach (int i in arr1)
+                { 
+                    Console.WriteLine("Element Value = " + i);
+                }
+            }
+        }      
+    }
+}
+
